4c453a55606a5db6d23ce6aeb29212973721342d
JiHawn <ghks4861@kookmin.ac.kr>
2021-08-07 16:37:27 +0900

    8/7 edit

--- prefetcher_test/prefetcher.c
+++ prefetcher_test/prefetcher.c
@@ -7,10 +7,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
 
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define BUFF_SIZE (1024*(EVENT_SIZE + 16))
+
+char* dirpath;
+int num_of_thread;
 
 double get_time() {
     double ms;
@@ -23,28 +27,85 @@ double get_time() {
     return s + ms;
 }
 
+void* prefetche(void* t) {
+    struct dirent *ent;
+    int fd, size;
+    int* num = (int *)t;
+    DIR* dir = opendir(dirpath);
+    char* targetfile = malloc(sizeof(char) * 255);
+    if(dir == NULL) {
+        perror("failed openning directroy");
+        exit(-1);
+    }
+    int i = 1;
+    while((ent = readdir(dir)) != NULL) {
+        if(!strncmp(ent->d_name, ".", 1)) {
+            i++;
+            continue;
+        }
+        if(i % num_of_thread != *num) {
+            i++;
+            continue;
+        }
+        strcat(targetfile, dirpath);
+        strcat(targetfile, ent->d_name);
+        fd = open(targetfile, O_RDONLY);
+        if(fd < 0) {
+            perror("failed caching file");
+            exit(-1);
+        }
+        size = lseek(fd, 0, SEEK_END);
+        lseek(fd, 0, SEEK_SET);
+        printf("%lf\n", get_time());
+        posix_fadvise(fd, 0, size, POSIX_FADV_WILLNEED);
+        // printf("Thread%d: %s\n", *num, targetfile);
+        
+        close(fd);
+        memset(targetfile, 0, sizeof(char) * 255);
+        i++;
+    }
+    free(targetfile);
+}
+
 void main(int argc, char* argv[]) {
     int fd, ifd = inotify_init();
     int wd, rc ,size;
     int flag = 0;
     double start, end;
     char buffer[BUFF_SIZE];
-    char target[20];
     struct dirent *ent;
     DIR *dir;
     FILE *pfd;
 
     if(argc == 1) {
-        printf("input directory name!\n");
+        printf("input options(-f, -t)\n");
         return;
     }
-    strcat(argv[1], "kb/");
-    strcat(target, argv[1]);
-    // printf("PID: %d\n", getpid());
 
-    wd = inotify_add_watch(ifd, target, IN_ACCESS);
+    int i = 1;
+    while(argv[i]) {
+        if(argv[i][0] == '-') {
+            if(argv[i][1] == 'f') {
+                dirpath = argv[i+1];
+            }
+            else if(argv[i][1] == 't') {
+                num_of_thread = atoi(argv[i+1]);
+            }
+            else {
+                printf("option error\n");
+                return;
+            }
+        }
+        else {
+            printf("input options (-f, -t)\n");
+        }
+        i += 2;
+    }
+    strcat(dirpath, "/");
+    pthread_t p_thread[num_of_thread];
+    wd = inotify_add_watch(ifd, dirpath, IN_ACCESS);
     if(wd == -1) {
-        perror("Worng directory name");
+        perror("Wrong directory name");
         return;
     }
 
@@ -55,38 +116,21 @@ void main(int argc, char* argv[]) {
             struct inotify_event *event = (struct inotify_event*)&buffer[i];
             if(event->len) {
                 if(event->mask & IN_ACCESS) {
-                    dir = opendir(target);
-                    if(dir == NULL) {
-                        perror("failed openning directroy");
-                        inotify_rm_watch(ifd, wd);
-                        return;
+                    int args[num_of_thread];
+                    for(int j=0; j<num_of_thread; j++) {
+                        args[j] = j;
+                        int thr = pthread_create(&p_thread[j], NULL, prefetche, (void *)&args[j]);
+                        if(thr<0) {
+                            perror("thread create error:");
+                            exit(0);
+                        }
                     }
-                    while((ent = readdir(dir)) != NULL) {
-                            if(!strncmp(ent->d_name, ".", 1)) continue;
-                            char *targetfile = malloc(sizeof(char) * 30);
-                            strcat(targetfile, target);
-                            strcat(targetfile, ent->d_name);
-                            fd = open(targetfile, O_RDONLY);
-                            if(fd < 0) {
-                                perror("failed caching file");
-                                inotify_rm_watch(ifd, wd);
-                                return;
-                            }
-                            size = lseek(fd, 0, SEEK_END);
-                            lseek(fd, 0, SEEK_SET);
-                            // if(!flag) {
-                            //     start = get_time();
-                            //     flag = 1;
-                            // } 
-                            printf("%lf\n", get_time());
-                            posix_fadvise(fd, 0, size, POSIX_FADV_WILLNEED);
-                            
-                            close(fd);
-                            free(targetfile);
+                    
+                    for(int j=0; j<num_of_thread; j++) {
+                        pthread_join(p_thread[j], NULL);
                     }
                     // end = get_time();
                     // printf("%lf\n", end-start);
-
                     break;
                 }
             }
