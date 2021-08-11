146034f15e39e31358cfb9e4a964876754da76c5
JiHawn <ghks4861@kookmin.ac.kr>
2021-08-11 19:52:23 +0900

    Delete looping_test.sh

--- prefetcher_test/looping_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for ((i=0; i<${1}; i++)); do
-    echo 1 | sudo tee /proc/sys/vm/drop_caches
-    ./read_4kb
-	echo 1 | sudo tee /proc/sys/vm/drop_caches
-	./prefetcher_4kbs & (sleep 1 && ./read_4kb)
-done
