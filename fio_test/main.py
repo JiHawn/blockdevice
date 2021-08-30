5246b77f065757f1b0af06f5f24a36f2328bb744
JiHawn <ghks4861@kookmin.ac.kr>
2021-08-30 11:15:19 +0900

    edit main.py

--- fio_test/main.py
+++ fio_test/main.py
@@ -26,25 +26,31 @@ ra_start = float(logs[-1][6])
 ra_end = 0
 r_start = float(logs[-1][6])
 r_end = 0
+rm = 0
+ra = 0
+r = 0
 
 for i in logs:
     ts = float(i[6])
     if i[2] == 'RM':
+        rm += 1
         if ts < rm_start:
             rm_start = ts
         if ts > rm_end:
             rm_end = ts
     elif i[2] == 'RA':
+        ra += 1
         if ts < ra_start:
             ra_start = ts
         if ts > rm_end:
             ra_end = ts
     elif i[2] == 'R':
+        r += 1
         if ts < r_start:
             r_start = ts
         if ts > r_end:
             r_end = ts
 
-print('RM spend time:', (rm_end - rm_start) * 1000)
-print('RA spend time:', (ra_end - ra_start) * 1000)
-print('R spend time:', (r_end - r_start) * 1000)
+print('# of RM:', str(rm), 'RM spend time:', (rm_end - rm_start) * 1000)
+print('# of RA:', str(ra), 'RA spend time:', (ra_end - ra_start) * 1000)
+print('# of R:', str(r), 'R spend time:', (r_end - r_start) * 1000)
