9f4b81dacbc851f6d770084ef25d7859248d6db7
jihawn <ghks4861@kookmin.ac.kr>
2021-09-23 19:21:25 +0900

    09/23 commit

--- fio_test/main2.py
+++ fio_test/main2.py
@@ -66,29 +66,56 @@ while True:
 # print(f"scenario D: I/O average time = {sum(complete_time)/len(complete_time)*1000}")
 # print(f"scenario D: sum of I/O time = {sum(complete_time)}")
 
-issue = {}
-complete_time = []
-for i in c_logs:
-    if i[3] == 'D':
-        issue[i[4]] = float(i[6])
-    elif i[3] == "C":
-        complete_time.append(float(i[6]) - issue[i[4]])
-        del issue[i[4]]
-
-# print(f"scenario C: D to C gap = {sum(complete_time)/len(complete_time)*1000}")
-print(f"scenario C: sum of issue to complete time = {sum(complete_time)}s")
-
-issue = {}
-complete_time = []
-for i in d_logs:
-    if i[3] == 'D':
-        issue[i[4]] = float(i[6])
-    elif i[3] == "C":
-        complete_time.append(float(i[6]) - issue[i[4]])
-        del issue[i[4]]
-
-# print(f"scenario D: I/O average time with RM = {sum(complete_time) / len(complete_time)*1000}")
-print(f"scenario D: sum of issue to complete time = {sum(complete_time)}s")
+# issue = {}
+# complete_time = []
+# for i in c_logs:
+#     if i[5] != '4096' and i[2] == "RA":
+#         if i[3] == 'D':
+#             issue[i[4]] = float(i[6])
+#         elif i[3] == "C":
+#             complete_time.append(float(i[6]) - issue[i[4]])
+#             del issue[i[4]]
+#
+# # print(f"scenario C: D to C gap = {sum(complete_time)/len(complete_time)*1000}")
+# print(f"scenario C: sum of metadata access time = {sum(complete_time)}s")
+#
+# issue = {}
+# complete_time = []
+# for i in d_logs:
+#     if i[5] != '4096' and i[2] == "RA":
+#         if i[3] == 'D':
+#             issue[i[4]] = float(i[6])
+#         elif i[3] == "C":
+#             complete_time.append(float(i[6]) - issue[i[4]])
+#             del issue[i[4]]
+#
+# # print(f"scenario D: I/O average time with RM = {sum(complete_time) / len(complete_time)*1000}")
+# print(f"scenario D: sum of metadata access time = {sum(complete_time)}s")
+#
+#
+# issue = {}
+# complete_time = []
+# for i in c_logs:
+#     if i[3] == 'D':
+#         issue[i[4]] = float(i[6])
+#     elif i[3] == "C":
+#         complete_time.append(float(i[6]) - issue[i[4]])
+#         del issue[i[4]]
+#
+# # print(f"scenario C: D to C gap = {sum(complete_time)/len(complete_time)*1000}")
+# print(f"scenario C: sum of issue to complete time = {sum(complete_time)}s")
+#
+# issue = {}
+# complete_time = []
+# for i in d_logs:
+#     if i[3] == 'D':
+#         issue[i[4]] = float(i[6])
+#     elif i[3] == "C":
+#         complete_time.append(float(i[6]) - issue[i[4]])
+#         del issue[i[4]]
+#
+# # print(f"scenario D: I/O average time with RM = {sum(complete_time) / len(complete_time)*1000}")
+# print(f"scenario D: sum of issue to complete time = {sum(complete_time)}s")
 
 complete = 0
 time = []
@@ -100,7 +127,8 @@ for i in c_logs:
             time.append(float(i[6]) - complete)
 
 # print(f"scenario C: average of distance of I/O = {sum(time)/len(time)*1000}")
-print(f"scenario C: sum of complete to next issue time = {sum(time)}s")
+print(f"scenario C: minimum of complete to next issue time = {min(time)}s")
+print(f"scenario C: maximum of complete to next issue time = {max(time)}s")
 
 complete = 0
 time = []
@@ -112,4 +140,5 @@ for i in d_logs:
             time.append(float(i[6]) - complete)
 
 # print(f"scenario D: average of distance of I/O = {sum(time) / len(time)*1000}")
-print(f"scenario D: sum of complete to next issue time = {sum(time)}s")
+print(f"scenario D: minimum of complete to next issue time = {min(time)}s")
+print(f"scenario D: maximum of complete to next issue time = {max(time)}s")
